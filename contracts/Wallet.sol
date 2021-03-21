b15388ea7760bac8e0d01c19717a389b76720acd
Piotr Misiak <p.misiak@gmail.com>
2021-03-21 15:23:09 +0100

    add possibilty to deposit and withdraw eth

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -13,10 +13,11 @@ contract Wallet is Ownable {
         address tokenAddress;
     }
 
-    mapping(bytes32 => Token) public tokenMapping;
     bytes32[] public tokenList;
-
+    mapping(bytes32 => Token) public tokenMapping;
     mapping(address => mapping(bytes32 => uint256)) public balances;
+
+    bytes32 internal ethTicker = bytes32("ETH");
 
     modifier tokenExists(bytes32 _ticker) {
         require(tokenMapping[_ticker].tokenAddress != address(0), "Token does not exist");
@@ -28,16 +29,24 @@ contract Wallet is Ownable {
         tokenList.push(_ticker);
     }
 
+    function depositEth() external payable {
+        balances[msg.sender][ethTicker] = balances[msg.sender][ethTicker].add(msg.value);
+    }
+
+    function withdrawEth(uint256 _amount) external {
+        require(balances[msg.sender][ethTicker] >= _amount);
+        balances[msg.sender][ethTicker] = balances[msg.sender][ethTicker].sub(_amount);
+        msg.sender.transfer(_amount);
+    }
+
     function deposit(uint _amount, bytes32 _ticker) tokenExists(_ticker) external {
         require(_amount > 0, "Insufficient amount deposited");
-
         IERC20(tokenMapping[_ticker].tokenAddress).transferFrom(msg.sender, address(this), _amount);
         balances[msg.sender][_ticker] = balances[msg.sender][_ticker].add(_amount);
     }
 
     function withdraw(uint _amount, bytes32 _ticker) tokenExists(_ticker) external {
-        require(balances[msg.sender][_ticker] >= _amount, "Balance not sufficient");
-    
+        require(balances[msg.sender][_ticker] >= _amount, "Balance not sufficient");   
         balances[msg.sender][_ticker] = balances[msg.sender][_ticker].sub(_amount);
         IERC20(tokenMapping[_ticker].tokenAddress).transfer(msg.sender, _amount);
     }
--- test/Wallet.js
+++ test/Wallet.js
@@ -6,43 +6,63 @@ const truffleAssert = require('truffle-assertions');
 contract("Wallet", accounts => {
     let wallet;
     let link;
+    let linkTicker;
 
     before(async() => {
         wallet = await Wallet.deployed();
         link = await Link.deployed();
+        linkTicker = web3.utils.fromUtf8("LINK");
     })
 
     it("should only be possible for owner to add tokens", async() => {
         await truffleAssert.passes(
-            wallet.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[0]})
+            wallet.addToken(linkTicker, link.address, {from: accounts[0]})
         );
         await truffleAssert.reverts(
-            wallet.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[1]})
+            wallet.addToken(linkTicker, link.address, {from: accounts[1]})
         );
     })
 
-    it("should handle faulty deposits correctly", async() => {
+    it("should handle faulty token deposits correctly", async() => {
         await truffleAssert.reverts(
-            wallet.deposit(100, web3.utils.fromUtf8("LINK"))
+            wallet.deposit(100, linkTicker)
         );
     })
 
-    it("should handle deposits correctly", async() => {
+    it("should handle token deposits correctly", async() => {
         await link.approve(wallet.address, 500);
-        await wallet.deposit(100, web3.utils.fromUtf8("LINK"));
-        let balance = await wallet.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+        await wallet.deposit(100, linkTicker);
+        let balance = await wallet.balances(accounts[0], linkTicker);
         assert.equal(balance.toNumber(), 100);
     })
 
-    it("should handle faulty withdrawals correctly", async() => {
+    it("should handle faulty token withdrawals correctly", async() => {
         await truffleAssert.reverts(
-            wallet.withdraw(200, web3.utils.fromUtf8("LINK"))
+            wallet.withdraw(200, linkTicker)
         );
     })
 
-    it("should handle withdrawals correctly", async() => {
+    it("should handle token withdrawals correctly", async() => {
         await truffleAssert.passes(
-            wallet.withdraw(100, web3.utils.fromUtf8("LINK"))
+            wallet.withdraw(100, linkTicker)
+        );
+    })
+
+    it("should handle eth deposits correctly", async() => {
+        await truffleAssert.passes(
+            wallet.depositEth({value: 100})
+        );
+    })
+
+    it("should handle eth faulty withdrawals correctly", async() => {
+        await truffleAssert.reverts(
+            wallet.withdrawEth(200)
+        );
+    })
+
+    it("should handle eth withdrawals correctly", async() => {
+        await truffleAssert.passes(
+            wallet.withdrawEth(100)
         );
     })
 
