1360dc67079a7e1fc55126c23f89635e7a3fef12
Piotr Misiak <p.misiak@gmail.com>
2021-03-21 15:24:22 +0100

    add tests for limit order

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -5,6 +5,8 @@ import "./Wallet.sol";
 
 contract Dex is Wallet {
 
+    using SafeMath for uint256;
+
     enum Side {
         BUY,
         SELL
@@ -22,13 +24,17 @@ contract Dex is Wallet {
 
     mapping(bytes32 => mapping(uint => Order[])) orderBook;
 
-    function getOrderBook(bytes32 _ticker, Side _side) view public returns(Order[] memory) {
+    function getOrderBook(bytes32 _ticker, Side _side) public view returns(Order[] memory) {
         return orderBook[_ticker][uint(_side)];
     }
 
-    /*
-    function createLimitOrder() {}
+    function createLimitOrder(Side _side, bytes32 _ticker, uint256 _amount, uint256 _price) public {
+        if (_side == Side.BUY) {
+            require(balances[msg.sender][ethTicker] >= _amount.mul(_price));
+        }
+    }
 
-    function createMarketOrder() {}
-    */
+    function createMarketOrder(Side _side, bytes32 _ticker, uint256 _amount, uint256 _price) public {
+        //TODO
+    }
 }
--- test/Dex.js
+++ test/Dex.js
@@ -6,10 +6,63 @@ const truffleAssert = require('truffle-assertions');
 contract("Dex", accounts => {
     let dex;
     let link;
+    let linkTicker;
 
     before(async() => {
         dex = await Dex.deployed();
         link = await Link.deployed();
-    })
+        linkTicker = web3.utils.fromUtf8("LINK");
+        await dex.addToken(linkTicker, link.address, {from: accounts[0]})
+    });
+
+    it("should throw error if ETH balance is less than BUY limit order amount", async() => {
+        await truffleAssert.reverts(
+            dex.createLimitOrder(0, linkTicker, 1, 10)
+        );
+        await dex.depositEth({value: 10});
+        await truffleAssert.passes(
+            dex.createLimitOrder(0, linkTicker, 1, 10)
+        );
+    });
     
+    it("should throw error if token balance is less than SELL limit order amount", async() => {
+        await truffleAssert.reverts(
+            dex.createLimitOrder(1, linkTicker, 1, 10)
+        );
+        await link.approve(dex.address, 1);
+        await dex.deposit(1, linkTicker);
+        await truffleAssert.passes(
+            dex.createLimitOrder(1, linkTicker, 1, 10)
+        );
+    });
+
+    it("BUY order book should be sorted in increasing manner", async() => {
+        await dex.depositEth({value: 500});
+
+        await dex.createLimitOrder(0, linkTicker, 1, 300);
+        await dex.createLimitOrder(0, linkTicker, 1, 100);
+        await dex.createLimitOrder(0, linkTicker, 1, 200);
+
+        let buyOrderBook = await dex.getOrderBook(linkTicker, 0);
+        assert(buyOrderBook.length > 0, "incorrect size of buy order book");
+        for (let i = 0; i < buyOrderBook.length - 1; i++) {
+            assert(buyOrderBook[i].price <= buyOrderBook[i + 1].price, "incorrect order in buy order book");
+        }
+    });
+
+    it("SELL order book should be sorted in decreasing manner", async() => {
+        await link.approve(dex.address, 3);
+        await dex.deposit(3, linkTicker);
+
+        await dex.createLimitOrder(1, linkTicker, 1, 300);
+        await dex.createLimitOrder(1, linkTicker, 1, 100);
+        await dex.createLimitOrder(1, linkTicker, 1, 200);
+
+        let sellOrderBook = await dex.getOrderBook(linkTicker, 1);
+        assert(sellOrderBook.length > 0, "incorrect size of sell order book");
+        for (let i = 0; i < sellOrderBook.length - 1; i++) {
+            assert(sellOrderBook[i].price >= sellOrderBook[i + 1].price, "incorrect order in sell order book");
+        }
+    });
+
 })
