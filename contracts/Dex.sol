f4edc9b8834098b1076d872462c7f176719925f0
Piotr Misiak <p.misiak@gmail.com>
2021-03-21 21:23:07 +0100

    implement limit order logic

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -15,14 +15,14 @@ contract Dex is Wallet {
     struct Order {
         uint id;
         address trader;
-        bool buyOrder;
         Side side;
         bytes32 ticker;
         uint amount;
         uint price;
     }
 
-    mapping(bytes32 => mapping(uint => Order[])) orderBook;
+    uint public nextOrderId = 0;
+    mapping(bytes32 => mapping(uint => Order[])) public orderBook;
 
     function getOrderBook(bytes32 _ticker, Side _side) public view returns(Order[] memory) {
         return orderBook[_ticker][uint(_side)];
@@ -32,6 +32,30 @@ contract Dex is Wallet {
         if (_side == Side.BUY) {
             require(balances[msg.sender][ethTicker] >= _amount.mul(_price));
         }
+        if (_side == Side.SELL) {
+            require(balances[msg.sender][_ticker] >= _amount);
+        }
+        Order[] storage orders = orderBook[_ticker][uint(_side)];
+        orders.push(Order(nextOrderId, msg.sender, _side, _ticker, _amount, _price));
+        nextOrderId = nextOrderId.add(1);
+        sort(orders, _side);
+    }
+
+    function sort(Order[] storage _orders, Side _side) private {
+        for (uint i = _orders.length - 1; i > 0; i--) {
+            if (Side.BUY == _side && _orders[i].price < _orders[i - 1].price 
+            || Side.SELL == _side && _orders[i].price > _orders[i - 1].price) {
+                swap(_orders, i, i - 1);
+            } else {
+                break;
+            }
+        }
+    }
+
+    function swap(Order[] storage _orders, uint _i, uint _j) private {
+        Order memory temp = _orders[_i];
+        _orders[_i] = _orders[_j];
+        _orders[_j] = temp;
     }
 
     function createMarketOrder(Side _side, bytes32 _ticker, uint256 _amount, uint256 _price) public {
